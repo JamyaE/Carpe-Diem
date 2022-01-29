152ad763f0512b9164fbae4f7753c065e9cf26e9
DonatoK <donatokava@gmail.com>
2022-01-29 09:57:35 -0700

    example

---
+++ main.py
@@ -0,0 +1,76 @@
+# some examp;e python code linking what we learned in class to some new code
+# 0be used as a  learning example
+# By Donato Kava
+import subprocess
+#import subprocess
+import math
+
+
+print("module is running")
+
+# By default the python subpress.run() command on windows is running the
+# command prompt program that has a different santax from powershell.
+# (e.g "ls" = "dir")
+# if you want to find more commands here is the online resource.
+
+# The code below is more simple but since we learned powershell and its newer
+# I pnly list it so you can see the differnece and be aware.
+# list_files = subprocess.call("dir", shell=True)
+# print("The exit code was: %d" % list_files)
+
+list_files = subprocess.run(["powershell", "-command", "ls"])
+
+# CHALLANGE try to get this to run without errors showing up when the file
+# already exists
+
+# Make a new folder to place some files into
+list_files = subprocess.run(["powershell", "-command", "mkdir", "file_folder"])
+
+# Change the active director to the new folder, How could you skip this step?
+list_files = subprocess.run(["powershell", "-command", "cd", "./file_folder"])
+# Display the directory to show the change
+list_files = subprocess.run(["powershell", "-command", "ls"])
+
+# CHALLANGE try to make this range base on something else.
+for x in range(0, 13, 2):
+    # CHALLANGE Try changing New-item to common typos to see what errors you
+    # get (e.g try putting in new_item)
+
+    # *****************************************************
+    # ** Notice how i put the folder in new-item name.   **
+    #    The command is new-item ./file_folder/file#.txt **
+    # *****************************************************
+
+    # place command here
+    # this is a string contructor example below
+    list_files = subprocess.run(
+        ["powershell", "-command", "New-Item", "./file_folder/file_" + str(x) + ".txt"])
+    # Display the directory to show the change
+    list_files = subprocess.run(
+        ["powershell", "-command", "ls", "./file_folder"])
+    #conditional example
+    if(x == 2):
+        print("I only print the last time")
+        list_files = subprocess.run(
+                ["powershell", "-command", "mv", "./file_folder/file_" + str(x) + ".txt", "./"])
+        x = 3
+        print("for loop", x)
+    print(x)
+
+print("Module Finished")
+
+
+
+# sorting code
+
+# 4 3 2 7 1
+
+list = [4, 3, 2, 7, 1 ]
+
+x1 = 4
+x2 = 3
+x3 = 2
+x4=  7
+x5 = 1
+
+sortedlist = sort (x1, x2, x3 ,x4, x5)
